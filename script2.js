cdcc006a3f5f837b136a5f3d1bad944c07acd987
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-03-31 19:12:59 +0530

    Static Files and Error Handling Completed

--- script2.js
+++ script2.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); 
+app.use(express.static('./public'));
 
 app.get("/", function (req, res) {
-  res.render("index");    // don't mention .ejs
+    throw Error("Pata nahi bhai");    // don't mention .ejs
 });
 
-app.get("/contact", function (req, res) {
-    res.render("contact");    // don't mention .ejs
+app.get("/error", function (req, res, next) {
+    throw Error("Something went wrong");    // don't mention .ejs
   });
+
+app.get("/contact", function (req, res) {
+    res.render("contact", {name: "Vansh"});    // don't mention .ejs
+  });
+
+app.use(function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+      return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+  });  
 
 app.listen(3001);
